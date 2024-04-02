ca28957f3e7c6704b00dd3a271e0f6f9b3d0f2df
saintcyrATSSU <m_saintcyr@salemstate.edu>
2024-04-02 09:35:43 -0400

    added profile Page with username and bio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 
 // We import all the components we need in our app
 import Navbar from "./components/navbar";
+import ProfilePage from "./components/pages/profilePage";
 import LandingPage from "./components/pages/landingPage";
 import HomePage from "./components/pages/homePage";
 import Login from "./components/pages/loginPage";
@@ -11,6 +12,7 @@ import Signup from "./components/pages/registerPage";
 import PrivateUserProfile from "./components/pages/privateUserProfilePage";
 import { createContext, useState, useEffect } from "react";
 import getUserInfo from "./utilities/decodeJwt";
+
 
 export const UserContext = createContext();
 //test change
@@ -27,11 +29,13 @@ const App = () => {
       <Navbar />
       <UserContext.Provider value={user}>
         <Routes>
+          <Route path="/profilePage" element={<ProfilePage />} />
           <Route exact path="/" element={<LandingPage />} />
           <Route exact path="/home" element={<HomePage />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/signup" element={<Signup />} />
           <Route path="/privateUserProfile" element={<PrivateUserProfile />} />
+          
         </Routes>
       </UserContext.Provider>
     </>
---
+++ frontend/src/components/pages/profilePage.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import Card from 'react-bootstrap/Card';
+import axios from 'axios';
+
+function profilePage() {
+  const [profiles, setProfiles] = useState([]);
+
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios(
+        'http://localhost:8081/profile/getAll',
+      );
+      setProfiles(result.data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Profiles</h1>
+      {profiles.map(profile => (
+        <Card body outline color="success" className="mx-1 my-2" bg="danger" style={{ width: "30rem" }}
+      >
+        <Card.Body>
+        <Card.Title>profile</Card.Title>
+        <Card.Text>{profile.username}{profile.bio}</Card.Text>
+        </Card.Body>
+      </Card>
+      ))}
+
+    </div>
+  );
+}
+
+export default profilePage;
